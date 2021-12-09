765b52f03f54e9a5d0c9f525a3964dd3c86a2087
Tianyu <ztyame@gmail.com>
2021-12-09 17:48:00 -0500

    feat: websharper temp

---
+++ WebSharper/wwwroot/Scripts/WebSharper/TwitterClone.js
@@ -0,0 +1,68 @@
+(function(Global)
+{
+ "use strict";
+ var WebSharper,Client,TwitterClone_Templates,Concurrency,Remoting,AjaxRemotingProvider,UI,Var$1,Templating,Runtime,Server,ProviderBuilder,Handler,TemplateInstance,Client$1,Templates;
+ WebSharper=Global.WebSharper=Global.WebSharper||{};
+ Client=WebSharper.Client=WebSharper.Client||{};
+ TwitterClone_Templates=Global.TwitterClone_Templates=Global.TwitterClone_Templates||{};
+ Concurrency=WebSharper&&WebSharper.Concurrency;
+ Remoting=WebSharper&&WebSharper.Remoting;
+ AjaxRemotingProvider=Remoting&&Remoting.AjaxRemotingProvider;
+ UI=WebSharper&&WebSharper.UI;
+ Var$1=UI&&UI.Var$1;
+ Templating=UI&&UI.Templating;
+ Runtime=Templating&&Templating.Runtime;
+ Server=Runtime&&Runtime.Server;
+ ProviderBuilder=Server&&Server.ProviderBuilder;
+ Handler=Server&&Server.Handler;
+ TemplateInstance=Server&&Server.TemplateInstance;
+ Client$1=UI&&UI.Client;
+ Templates=Client$1&&Client$1.Templates;
+ Client.Main$19$20=function(rvReversed)
+ {
+  return function(e)
+  {
+   var b;
+   Concurrency.StartImmediate((b=null,Concurrency.Delay(function()
+   {
+    return Concurrency.Bind((new AjaxRemotingProvider.New()).Async("TwitterClone:WebSharper.Server.DoSomething:-2030603568",[e.Vars.Hole("texttoreverse").$1.Get()]),function(a)
+    {
+     rvReversed.Set(a);
+     return Concurrency.Zero();
+    });
+   })),null);
+  };
+ };
+ Client.Main=function()
+ {
+  var rvReversed,b,R,_this,t,p,i;
+  rvReversed=Var$1.Create$1("");
+  return(b=(R=rvReversed.get_View(),(_this=(t=new ProviderBuilder.New$1(),(t.h.push(Handler.EventQ2(t.k,"onsend",function()
+  {
+   return t.i;
+  },function(e)
+  {
+   var b$1;
+   Concurrency.StartImmediate((b$1=null,Concurrency.Delay(function()
+   {
+    return Concurrency.Bind((new AjaxRemotingProvider.New()).Async("TwitterClone:WebSharper.Server.DoSomething:-2030603568",[e.Vars.Hole("texttoreverse").$1.Get()]),function(a)
+    {
+     rvReversed.Set(a);
+     return Concurrency.Zero();
+    });
+   })),null);
+  })),t)),(_this.h.push({
+   $:2,
+   $0:"reversed",
+   $1:R
+  }),_this))),(p=Handler.CompleteHoles(b.k,b.h,[["texttoreverse",0]]),(i=new TemplateInstance.New(p[1],TwitterClone_Templates.mainform(p[0])),b.i=i,i))).get_Doc();
+ };
+ TwitterClone_Templates.mainform=function(h)
+ {
+  Templates.LoadLocalTemplates("main");
+  return h?Templates.NamedTemplate("main",{
+   $:1,
+   $0:"mainform"
+  },h):void 0;
+ };
+}(self));
